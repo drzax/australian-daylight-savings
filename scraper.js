edc9dd000253dfcb480c87be6df7faaee545c6fb
Simon Elvery <simon@elvery.net>
2015-08-12 13:33:19 +1000

    Rename location column to state and remove silly logging

--- scraper.js
+++ scraper.js
@@ -20,7 +20,7 @@ function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (location TEXT, start TEXT, end TEXT, notes TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS data (state TEXT, start TEXT, end TEXT, notes TEXT)");
 		callback(db);
 	});
 }
@@ -38,8 +38,10 @@ function fetchPage(url, callback) {
 }
 
 function run(db) {
+
 	// Use request to read in pages.
 	fetchPage("http://www.bom.gov.au/climate/averages/tables/dst_times.shtml", function (body) {
+
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
 		var data = [[],[],[],[]];
@@ -64,13 +66,12 @@ function run(db) {
 						}
 					});
 			});
-			// updateRow(db, value);
 		});
-		console.log(data);
+
 		data[0].forEach(function(d,i){
 			d.split(',').forEach(function(l){
 				l = l.trim();
-				db.run('INSERT INTO data (location, start, end, notes) VALUES ($location,$start,$end,$notes)', {
+				db.run('INSERT INTO data (state, start, end, notes) VALUES ($location,$start,$end,$notes)', {
 					$location: l,
 					$start: moment(data[1][i]+' 02:00 '+timezones[l], 'D/M/YYYY HH:mm ZZ').toISOString(),
 					$end: moment(data[2][i]+' 03:00 '+timezones[l], 'D/M/YYYY HH:mm ZZ').toISOString(),
